29e931d4d26cc8e8b5ec28349feeb0b657c828fd
Adam Monikowski <adam.piotr.monikowski@gmail.com>
2024-04-29 12:47:09 +0200

    Add default MVC project

---
+++ AspLocalizationDemo.MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace AspLocalizationDemo.MVC.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
